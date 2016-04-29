efe5292a3ccf5c5c05ce2f7d36e643dc4522f08b
Nick Ager <nick.ager@gmail.com>
2016-04-29 15:31:32 +0100

    added a couple of extension mixins
    namely:
    * `AnyErrorConverter`
    * `KeepInMemoryMixin`

--- NACommonUtils/Utilities/AnyError.swift
+++ NACommonUtils/Utilities/AnyError.swift
@@ -26,3 +26,19 @@ public struct AnyError : ErrorType {
         self.cause = cause
     }
 }
+
+/**
+ Protocol extension designed as a mix-in to add `asAnyError` to any `ErrorType`
+ 
+ See also:
+ - [Type erasure with AnyError](http://nickager.com/blog/2016/03/07/AnyError)
+ */
+public protocol AnyErrorConverter : ErrorType {
+    func asAnyError() -> AnyError;
+}
+
+public extension AnyErrorConverter {
+    func asAnyError() -> AnyError {
+        return AnyError(cause: self)
+    }
+}
