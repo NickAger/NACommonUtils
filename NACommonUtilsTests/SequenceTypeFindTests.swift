914e8db455d0d3649bf0ebbbee506fc1e5484c7b
Nick Ager <nick.ager@gmail.com>
2016-05-04 21:42:41 +0100

    added SequenceType+find

---
+++ NACommonUtilsTests/SequenceTypeFindTests.swift
@@ -0,0 +1,21 @@
+//
+//  SequenceTypeFindTests.swift
+//  NACommonUtils
+//
+//  Created by Nick Ager on 04/05/2016.
+//  Copyright © 2016 RocketBox Ltd. All rights reserved.
+//
+
+import XCTest
+
+class SequenceTypeFindTests: XCTestCase {
+    func testFind() {
+        let a1 = [1,2,3,4,5,6]
+        
+        let divisableBy3 = a1.find { $0 % 3 == 0}
+        XCTAssert(divisableBy3 == 3)
+        
+        let divisableBy7 = a1.find { $0 % 7 == 0}
+        XCTAssert(divisableBy7 == nil)
+    }
+}
