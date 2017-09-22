35ee30c24fa2d8b3fc022ae21196c8cad1db7707
Nick Ager <nick.ager@gmail.com>
2017-09-22 15:53:33 +0100

    Updated to Swift 4

--- NACommonUtilsTests/SequenceTypeFindTests.swift
+++ NACommonUtilsTests/SequenceTypeFindTests.swift
@@ -13,10 +13,10 @@ class SequenceTypeFindTests: XCTestCase {
     func testFind() {
         let a1 = [1,2,3,4,5,6]
         
-        let divisableBy3 = a1.find { $0 % 3 == 0}
+        let divisableBy3 = a1.first { $0 % 3 == 0}
         XCTAssert(divisableBy3 == 3)
         
-        let divisableBy7 = a1.find { $0 % 7 == 0}
+        let divisableBy7 = a1.first { $0 % 7 == 0}
         XCTAssert(divisableBy7 == nil)
     }
 }
